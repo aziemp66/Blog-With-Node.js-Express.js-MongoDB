ea26bae4061626b0487bffe4cd98973305ac1f0b
Azie Melza Pratama <aziemp66@gmail.com>
2022-01-12 15:05:58 +0700

    Fetching documents and displaying list

--- routes/blog.js
+++ routes/blog.js
@@ -11,8 +11,14 @@ router.get("/", function (req, res) {
     res.redirect("/posts");
 });
 
-router.get("/posts", function (req, res) {
-    res.render("posts-list");
+router.get("/posts", async function (req, res) {
+    const posts = await db
+        .getDb()
+        .collection("posts")
+        .find({})
+        .project({ title: 1, summary: 1, "author.name": 1 })
+        .toArray();
+    res.render("posts-list", { posts: posts });
 });
 
 router.get("/new-post", async function (req, res) {
@@ -32,7 +38,7 @@ router.post("/posts", async function (req, res) {
         summary: req.body.summary,
         body: req.body.content,
         date: new Date(),
-        authors: {
+        author: {
             id: authorId,
             name: author.name,
             email: author.email,
